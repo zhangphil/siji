4ba06babbd7163b20d860d6a4c80d124fd71042e
zhangphil <zhang.fei@msn.com>
2015-10-09 20:10:49 +0800

    new

--- src/hcb/tc/sj/HomeActivity.java
+++ src/hcb/tc/sj/HomeActivity.java
@@ -6,6 +6,8 @@ import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.design.widget.TabLayout.OnTabSelectedListener;
+import android.support.design.widget.TabLayout.Tab;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -13,7 +15,6 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 import hcb.tc.sj.fragments.QiangDanFragment;
-
 
 public class HomeActivity extends FragmentActivity {
 
@@ -30,10 +31,9 @@ public class HomeActivity extends FragmentActivity {
 
 		final Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
 		// mToolbar.setLogo(R.drawable.ic_launcher);
-		mToolbar.setTitle("待初始化");
+		mToolbar.setTitle(tabName[0]);
 		mToolbar.setTitleTextColor(Color.WHITE);
-
-
+		
 		MyViewPagerAdapter adapter = new MyViewPagerAdapter(this.getSupportFragmentManager());
 		ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
 		viewPager.setAdapter(adapter);
@@ -42,7 +42,25 @@ public class HomeActivity extends FragmentActivity {
 		tabLayout.setSmoothScrollingEnabled(true);
 
 		tabLayout.setupWithViewPager(viewPager);
-		//mSlidingTabLayout.setViewPager(viewPager);  
+		
+		OnTabSelectedListener onTabSelectedListener=new OnTabSelectedListener(){
+
+			@Override
+			public void onTabReselected(Tab arg0) {
+				
+			}
+
+			@Override
+			public void onTabSelected(Tab tab) {
+				int pos=tab.getPosition();
+				mToolbar.setTitle(tabName[pos]);
+			}
+
+			@Override
+			public void onTabUnselected(Tab arg0) {
+				
+			}};
+		tabLayout.setOnTabSelectedListener(onTabSelectedListener);
 	}
 
 	private class MyViewPagerAdapter extends FragmentPagerAdapter {
